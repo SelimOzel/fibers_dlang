69ac746e9cfac7b47c6ef6e5fb4f31c521993329
Selim Ozel <sozel@wpi.edu>
2021-10-16 22:57:38 +0200

    Weeeeiiiiird.

--- source/app.d
+++ source/app.d
@@ -1,29 +1,33 @@
 import std.stdio;
 import std.concurrency;
-import core.thread: Fiber;
+import core.thread;
 
-void DatabaseProgram() {
-	writeln("[Database program] upload bulk data. one time.");
+void MessageHandler() {
+	while(true) {
+		receive( 
+			(int message) {writeln(message);} 
+		);
+		writeln("[MessageHandler] message manager update.");
+	}
+}
+
+void DatabaseProgram(Tid msg_handler_TiD) {
+	writeln("[DatabaseProgram] upload bulk data. one time.");
 	int number_of_calls = 0;
     Fiber.yield();
     while(true) {
-    	writeln("[Database program] tried to update data. once per day.");
+    	writeln("[DatabaseProgram] tried to update data. once per day.");
     	number_of_calls += 1;
+    	send(msg_handler_TiD, 13);
     	if(number_of_calls == 5) {
+    		writeln("[DatabaseProgram] Fifth update of the day.");
     		number_of_calls = 0;
-    		// send message here.
+    		string msg = "database_updated";
+    		
+    		Thread.sleep( 1.seconds );
     	}
     	Fiber.yield();
     }
-}
-
-void Program_1() {
-	writeln("[Program_1] buy/sell, do some initial things.");
-	Fiber.yield();
-    while(true) {
-    	writeln("[Program_1] trying to update.");
-    	Fiber.yield();
-    }	
 }
 
 void main() {
@@ -31,7 +35,9 @@ void main() {
     scheduler.start( 
     {
         writeln("[main] Fiber application.");
-        auto f_1 = spawn( &DatabaseProgram );
-        auto f_2 = spawn( &Program_1 );
+        auto f_0 = spawn( &MessageHandler );
+        auto f_1 = spawn( &DatabaseProgram, f_0 );
+        send(f_0, 13);
+        send(f_0, 17);
     });
 }
