584694f1c8219ac8eb74bd9f6109420fb048c90f
Selim Ozel <sozel@wpi.edu>
2021-10-16 21:48:23 +0200

    Example with a fiber scheduler for database and program_1. Uses yield to sync both fibers. Trying to add message send/receive to this.

--- source/app.d
+++ source/app.d
@@ -1,36 +1,49 @@
 import std.stdio;
+import std.concurrency;
+import core.thread: Fiber;
+
+void DatabaseProgram() {
+	writeln("[Database program] upload bulk data. one time.");
+	int number_of_calls = 0;
+    Fiber.yield();
+    while(true) {
+    	writeln("[Database program] tried to update data. once per day.");
+    	number_of_calls += 1;
+    	if(number_of_calls == 5) {
+    		number_of_calls = 0;
+    		// send message here.
+    	}
+    	Fiber.yield();
+    }
+}
+
+void Program_1() {
+	writeln("[Program_1] buy/sell, do some initial things.");
+	Fiber.yield();
+    while(true) {
+    	writeln("[Program_1] trying to update.");
+    	Fiber.yield();
+    }	
+}
+
+void main() {
+    scheduler = new FiberScheduler;
+    scheduler.start( 
+    {
+        writeln("[main] Fiber application.");
+        auto f_1 = spawn( &DatabaseProgram );
+        auto f_2 = spawn( &Program_1 );
+    });
 
 /*
-Database Program
-Load bulk data
-Get into update mode
-Publish update status
+	writeln("[main] Fiber application.");
+	Fiber f_1 = new Fiber( &DatabaseProgram );
+	Fiber f_2 = new Fiber( &Program_1 );
+
+	while(true) {
+		f_1.call();
+		f_2.call();
+		send(f_2, 2);
+	}
 */
-
-/*
-Program 1
-Do initials things
-Buy/Sell
-Buy/Sell
-
-Get into update mode and wait for input
-Subscribe to database update status
-*/
-
-/*
-Program 2
-Subscribe to database update status
-Directly get into update mode and wait for input
-*/
-
-/*
-Program 3
-Subscribe to database update status
-Directly get into update mode and wait for input
-On exit handle a couple of things
-*/
-
-void main()
-{
-	writeln("Fiber application.");
 }
