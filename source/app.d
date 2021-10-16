99d83323a82d749a575e286a5bfe21df5a8c86e1
Selim Ozel <sozel@wpi.edu>
2021-10-16 13:38:35 +0200

    Added initial thoughts on how things should be.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,32 @@
 import std.stdio;
 
+/*
+Database Program
+Load bulk data
+Get into update mode
+*/
+
+/*
+Program 1
+Do initials things
+Buy/Sell
+Buy/Sell
+
+Get into update mode and wait for input
+*/
+
+/*
+Program 2
+Directly get into update mode and wait for input
+*/
+
+/*
+Program 3
+Directly get into update mode and wait for input
+On exit handle a couple of things
+*/
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	writeln("Fiber application.");
 }
