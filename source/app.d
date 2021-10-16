9255c7222701ec549542e4c9b6ba7cf4f3d64703
Selim Ozel <sozel@wpi.edu>
2021-10-16 13:58:55 +0200

    Publish/subscription will be needed.

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std.stdio;
 Database Program
 Load bulk data
 Get into update mode
+Publish update status
 */
 
 /*
@@ -13,15 +14,18 @@ Buy/Sell
 Buy/Sell
 
 Get into update mode and wait for input
+Subscribe to database update status
 */
 
 /*
 Program 2
+Subscribe to database update status
 Directly get into update mode and wait for input
 */
 
 /*
 Program 3
+Subscribe to database update status
 Directly get into update mode and wait for input
 On exit handle a couple of things
 */
